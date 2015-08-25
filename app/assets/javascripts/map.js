18335bc2ed44e857427ee406186648cb1b6196f1
Ali Abou-Zahr <ali.a.zahr@gmail.com>
2015-08-25 00:29:00 -0700

    added marker functionality to maps

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,55 @@
 var map;
+var markers = [];
+
 function initMap() {
+
+  var myLatlng = {lat: 34.1639064, lng: -118.4229371},
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: myLatlng,
     zoom: 8
   });
+
+  var marker = new google.maps.Marker({
+     position: myLatlng,
+     map: map,
+     title: 'Click to zoom'
+   });
+
+  map.addListener('click', function(e) {
+    marker.setMap(null);
+    deleteMarkers();
+    placeMarkerAndPanTo(e.latLng, map);
+
+  });
+
+  function placeMarkerAndPanTo(latLng, map) {
+
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+
+    map.panTo(latLng);
+    markers.push(marker);
+  }
+
+  function deleteMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+
+    markers = [];
+  }
 }
+
+// function placeMarkerAndPanTo(latLng, map) {
+
+//   var marker = new google.maps.Marker({
+//     position: latLng,
+//     map: map
+//   });
+
+//   map.panTo(latLng);
+//   markers.push(marker);
+// }
